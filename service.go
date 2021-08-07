86a51ce9f132be875c41841d450ae1d63fc1cc8f
Octopus Octopus <oct2pus@jade.moe>
2021-08-06 20:10:52 -0500

    work on CommonBehavior

--- service.go
+++ service.go
@@ -1,35 +1,42 @@
 package main
 
 import (
+	"context"
 	"net/http"
 	"net/url"
+
+	"github.com/go-fed/activity/pub"
+	"github.com/go-fed/activity/streams/vocab"
 )
 
 type service struct{}
 
 // CommonBehavior
 
-func (*service) AuthenticateGetInbox(c context.Contect,
+func (*service) AuthenticateGetInbox(c context.Context,
 	w http.ResponseWriter,
 	r *http.Request) (out context.Context, authenticated bool, err error) {
-	// TODO
-	return
+	// TODO: Actually authenticate this.
+	return r.Context(), true, nil
 }
 
-func (*myService) AuthenticateGetOutbox(c context.Context,
+func (*service) AuthenticateGetOutbox(c context.Context,
 	w http.ResponseWriter,
 	r *http.Request) (out context.Context, authenticated bool, err error) {
-	// TODO
-	return
+	// TODO: Actually authenticate this.
+
+	return r.Context(), true, nil
 }
 
-func (*myService) GetOutbox(c context.Context,
+func (*service) GetOutbox(c context.Context,
 	r *http.Request) (vocab.ActivityStreamsOrderedCollectionPage, error) {
 	// TODO
+	rc := r.Body
+	r.URL
 	return nil, nil
 }
 
-func (*myService) NewTransport(c context.Context,
+func (*service) NewTransport(c context.Context,
 	actorBoxIRI *url.URL,
 	gofedAgent string) (t pub.Transport, err error) {
 	// TODO
@@ -38,55 +45,55 @@ func (*myService) NewTransport(c context.Context,
 
 // FederatingProtocol
 
-func (*myService) PostInboxRequestBodyHook(c context.Context,
+func (*service) PostInboxRequestBodyHook(c context.Context,
 	r *http.Request,
-	activity Activity) (context.Context, error) {
+	activity pub.Activity) (context.Context, error) {
 	// TODO
 	return nil, nil
 }
 
-func (*myService) AuthenticatePostInbox(c context.Context,
+func (*service) AuthenticatePostInbox(c context.Context,
 	w http.ResponseWriter,
 	r *http.Request) (out context.Context, authenticated bool, err error) {
 	// TODO
 	return
 }
 
-func (*myService) Blocked(c context.Context,
+func (*service) Blocked(c context.Context,
 	actorIRIs []*url.URL) (blocked bool, err error) {
 	// TODO
 	return
 }
 
-func (*myService) FederatingCallbacks(c context.Context) (wrapped FederatingWrappedCallbacks, other []interface{}, err error) {
-	// TODO
+func (*service) FederatingCallbacks(c context.Context) (wrapped pub.FederatingWrappedCallbacks, other []interface{}, err error) {
+	// Empty
 	return
 }
 
-func (*myService) DefaultCallback(c context.Context,
-	activity Activity) error {
+func (*service) DefaultCallback(c context.Context,
+	activity pub.Activity) error {
 	// TODO
 	return nil
 }
 
-func (*myService) MaxInboxForwardingRecursionDepth(c context.Context) int {
+func (*service) MaxInboxForwardingRecursionDepth(c context.Context) int {
 	// TODO
 	return -1
 }
 
-func (*myService) MaxDeliveryRecursionDepth(c context.Context) int {
+func (*service) MaxDeliveryRecursionDepth(c context.Context) int {
 	// TODO
 	return -1
 }
 
-func (*myService) FilterForwarding(c context.Context,
+func (*service) FilterForwarding(c context.Context,
 	potentialRecipients []*url.URL,
-	a Activity) (filteredRecipients []*url.URL, err error) {
+	a pub.Activity) (filteredRecipients []*url.URL, err error) {
 	// TODO
 	return
 }
 
-func (*myService) GetInbox(c context.Context,
+func (*service) GetInbox(c context.Context,
 	r *http.Request) (vocab.ActivityStreamsOrderedCollectionPage, error) {
 	// TODO
 	return nil, nil
